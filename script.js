476bc50eff268f3151f9bce94b69cd3c5d4239c5
Am706870 <88397078+Am706870@users.noreply.github.com>
2024-12-13 20:23:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(n){
+	if((n%4==0 && n%400!=0) || (n%100==0)){
+		return "366"
+	}else{
+		return "365"
+	}
+}
+let n = 245
+console.log(daysOfAYear(n))
